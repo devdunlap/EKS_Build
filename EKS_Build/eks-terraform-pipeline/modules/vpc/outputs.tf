cf2d2ad2a8676d3429724cb81b03c05504ecc22c
devdunlap <dev.dunlap.andrew@gmail.com>
2025-08-25 22:29:56 -0500

    added outputs tfvars

---
+++ EKS_Build/eks-terraform-pipeline/modules/vpc/outputs.tf
@@ -0,0 +1,39 @@
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = aws_vpc.this.id
+}
+
+output "vpc_cidr_block" {
+  description = "The CIDR block of the VPC"
+  value       = aws_vpc.this.cidr_block
+}
+
+output "public_subnet_ids" {
+  description = "List of IDs of the public subnets"
+  value       = aws_subnet.public[*].id
+}
+
+output "public_subnet_cidr_blocks" {
+  description = "List of CIDR blocks of the public subnets"
+  value       = aws_subnet.public[*].cidr_block
+}
+
+output "internet_gateway_id" {
+  description = "The ID of the Internet Gateway"
+  value       = aws_internet_gateway.this.id
+}
+
+output "public_route_table_id" {
+  description = "ID of the public route table"
+  value       = aws_route_table.public.id
+}
+
+output "availability_zones" {
+  description = "List of availability zones used by the subnets"
+  value       = data.aws_availability_zones.available.names
+}
+
+output "vpc_name" {
+  description = "Name tag of the VPC"
+  value       = var.vpc_name
+}
